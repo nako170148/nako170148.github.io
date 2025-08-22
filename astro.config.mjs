141a631e1a2c0a318eb7f54236fde416b9889a9d
nako170148 <yui.nako1205@gmail.com>
2025-08-22 19:51:02 +0900

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://<username>.github.io", // あなたの GitHub ユーザー名
+  base: "", // ユーザーページ型なので空
+});
