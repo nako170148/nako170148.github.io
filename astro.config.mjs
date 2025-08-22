7fad374f7f8a0f265f848e3eb1b7c79d4b985f2f
nako170148 <yui.nako1205@gmail.com>
2025-08-22 20:27:29 +0900

    edit astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://<username>.github.io", // あなたの GitHub ユーザー名
+  site: "https://nako170148.github.io", // あなたの GitHub ユーザー名
   base: "", // ユーザーページ型なので空
 });
